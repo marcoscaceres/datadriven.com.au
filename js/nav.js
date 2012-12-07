df27e1af6a16b0ea598e7aec9f039b1e555992a9
Marcos Caceres <marcos.caceres@wacapps.net>
2012-12-07 01:57:18 +0000

    Removed comments and other stuff I was playing around with

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,15 @@
 (function(window){
 	var services = document.getElementById("services"),
 		aboutme = document.getElementById("aboutme");
-		//orient = document.getElementById("orient"),
-		//oldGamma = 0;
-	
-	
-
+		
 	window.addEventListener("DOMContentLoaded", doMagic);
-	//window.addEventListener("deviceorientation", handleOrientation, true);
-	
+
 	function doMagic(){
 		updateLayout();
 		window.document.documentElement.setAttribute("class", "js");
 		window.addEventListener("hashchange", updateLayout);
 	}
+
 	function updateLayout(e){
 		if(window.location.hash === "#services/"){
 			services.className = "show";
@@ -23,13 +19,4 @@
 		services.className = "hide";
 		aboutme.className = "show";
 	}
-	/*
-	function handleOrientation(e){
-		var newGamma = Math.floor(e.gamma);
-		if(newGamma && (newGamma !== oldGamma)){
-			orient.style.webkitTransform = "rotate("+orientationDiff+"deg)";
-		}
-		oldGamma = newGamma;
-	}
-	*/
 }(this));
